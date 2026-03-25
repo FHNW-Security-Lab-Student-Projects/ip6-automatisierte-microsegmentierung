335c977d99de006696a82aea425aaaf52f92ea38
Martin Vogt <martin.vogt1@students.fhnw.ch>
2026-03-25 12:01:47 +0100

    Add basic analysis module
    Closes #7

--- prototype/scripts/run_pipeline.py
+++ prototype/scripts/run_pipeline.py
@@ -1,5 +1,6 @@
 from microseg.ingestion.csv_loader import load_nodes, load_traffic
 from microseg.graph.graph_builder import build_graph, print_graph_summary
+from microseg.analysis.analyzer import analyze_graph, print_top_nodes, inspect_node, inspect_edge
 from microseg.visualization.renderers.pyvis_renderer import visualize_graph
 
 
@@ -14,6 +15,21 @@ def main():
     # Build Graph
     # ---------------------------
     G = build_graph(nodes, traffic)
+    
+    # ---------------------------
+    # Analyze Graph
+    # ---------------------------
+    analysis = analyze_graph(G)
+    node_metrics = analysis["node_metrics"]
+    
+    print_top_nodes(node_metrics, key="degree")
+    # print_top_nodes(node_metrics, key="in_degree")
+    # print_top_nodes(node_metrics, key="out_degree")
+    # print_top_nodes(node_metrics, key="weighted_degree")
+    # print_top_nodes(node_metrics, key="betweenness")
+    
+    # inspect_node(G, analysis, "app01")
+    # inspect_edge(G, analysis, "app01", "db01")
 
     # ---------------------------
     # Debug Output
